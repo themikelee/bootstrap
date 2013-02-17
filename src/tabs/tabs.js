cf6fa9a09a3eaa3f9568c6d25f04f66d46c4150f
Andy Joslin <andytjoslin@gmail.com>
2013-02-17 13:07:37 -0500

    Tabs approach one

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -35,13 +35,21 @@ angular.module('ui.bootstrap.tabs', [])
     replace: true
   };
 })
+.controller('PaneController', ['$scope', function($scope) {
+  this.setHeading = function(element) {
+    $scope.headingElement = element;
+  };
+}])
 .directive('pane', ['$parse', function($parse) {
   return {
     require: '^tabs',
     restrict: 'EA',
     transclude: true,
-    scope:{
-      heading:'@'
+    templateUrl: 'template/tabs/pane.html',
+    replace: true,
+    controller: 'PaneController',
+    scope: {
+      heading: '@'
     },
     link: function(scope, element, attrs, tabsCtrl) {
       var getSelected, setSelected;
@@ -68,8 +76,45 @@ angular.module('ui.bootstrap.tabs', [])
       scope.$on('$destroy', function() {
         tabsCtrl.removePane(scope);
       });
-    },
-    templateUrl: 'template/tabs/pane.html',
-    replace: true
+    }
+  };
+}])
+.directive('paneHeading', ['$timeout', '$compile', function($timeout, $compile) {
+  return {
+    restrict: 'EA',
+    transclude: true, //We'll take the contents of this and use it as the heading
+    template: '', //basically remove this element
+    replace: true,
+    compile: function(elm, attrs, transclude) {
+      return function postLink(scope, elm, attrs) {
+        // If we do `require: '^pane' or require: 'pane' it simply cannot find
+        // the pane controller. Probably because the <pane-heading> element is
+        // compiled before its parent <pane> element (?)
+        // I also tried doing `terminal: true` on the paneHeading, but that
+        // didn't make any difference since terminal really only effects
+        // the order of directives-on-same-element compilation.
+        //
+        // For now, to get around the problem we have a $timeout so the paneHeading
+        // can wait until the pane compiles.
+        $timeout(function() { 
+          var pane = elm.parent().scope();
+          pane.headingElement = transclude(scope);
+        });
+      };
+    }
+  };
+}])
+.directive('paneHeadingTransclude', [function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elm, attrs) {
+      var pane = scope.$eval(attrs.paneHeadingTransclude);
+      pane.$watch('headingElement', function(heading) { 
+        if (heading) {
+          elm.html('');
+          elm.append(heading);
+        }
+      });
+    }
   };
 }]);
