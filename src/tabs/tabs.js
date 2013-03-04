a2d88091edfd86bda13c2606c025907ec28f31f3
Andy Joslin <andytjoslin@gmail.com>
2013-03-03 19:42:30 -0500

    refactor(tabs): Rework tabs to transclude to the headings

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -1,5 +1,7 @@
 angular.module('ui.bootstrap.tabs', [])
-.controller('TabsController', ['$scope', '$element', function($scope, $element) {
+
+.controller('TabsController', ['$scope', function($scope) {
+  var ctrl = this;
   var panes = $scope.panes = [];
 
   this.select = $scope.select = function selectPane(pane) {
@@ -7,6 +9,7 @@ angular.module('ui.bootstrap.tabs', [])
       pane.selected = false;
     });
     pane.selected = true;
+    ctrl.selectedPane = pane;
   };
 
   this.addPane = function addPane(pane) {
@@ -25,7 +28,8 @@ angular.module('ui.bootstrap.tabs', [])
     }
   };
 }])
-.directive('tabs', function() {
+
+.directive('tabs', [function() {
   return {
     restrict: 'EA',
     transclude: true,
@@ -34,42 +38,105 @@ angular.module('ui.bootstrap.tabs', [])
     templateUrl: 'template/tabs/tabs.html',
     replace: true
   };
-})
+}])
+
+.controller('PaneController', ['$scope', function($scope) {
+}])
+
 .directive('pane', ['$parse', function($parse) {
   return {
     require: '^tabs',
     restrict: 'EA',
     transclude: true,
-    scope:{
-      heading:'@'
-    },
-    link: function(scope, element, attrs, tabsCtrl) {
-      var getSelected, setSelected;
-      scope.selected = false;
-      if (attrs.active) {
-        getSelected = $parse(attrs.active);
-        setSelected = getSelected.assign;
-        scope.$watch(
-          function watchSelected() {return getSelected(scope.$parent);},
-          function updateSelected(value) {scope.selected = value;}
-        );
-        scope.selected = getSelected ? getSelected(scope.$parent) : false;
-      }
-      scope.$watch('selected', function(selected) {
-        if(selected) {
-          tabsCtrl.select(scope);
-        }
-        if(setSelected) {
-          setSelected(scope.$parent, selected);
-        }
-      });
-
-      tabsCtrl.addPane(scope);
-      scope.$on('$destroy', function() {
-        tabsCtrl.removePane(scope);
-      });
-    },
     templateUrl: 'template/tabs/pane.html',
-    replace: true
+    replace: true,
+    scope: {
+      heading: '@'
+    },
+    controller: 'PaneController',
+    compile: function(elm, attrs, transclude) {
+      return function link(scope, elm, attrs, tabsCtrl) {
+
+        var getSelected, setSelected;
+        scope.selected = false;
+        if (attrs.active) {
+          getSelected = $parse(attrs.active);
+          setSelected = getSelected.assign;
+          scope.$watch(
+            function watchSelected() {return getSelected(scope.$parent);},
+            function updateSelected(value) {scope.selected = value;}
+          );
+          scope.selected = getSelected ? getSelected(scope.$parent) : false;
+        }
+        scope.$watch('selected', function(selected) {
+          if(selected) {
+            tabsCtrl.select(scope);
+          }
+          if(setSelected) {
+            setSelected(scope.$parent, selected);
+          }
+        });
+
+        scope.paneContent = transclude(scope, angular.noop);
+
+        tabsCtrl.addPane(scope);
+        scope.$on('$destroy', function() {
+          tabsCtrl.removePane(scope);
+        });
+
+        scope.select = function() {
+          tabsCtrl.select(scope);
+        };
+      };
+    }
+  };
+}])
+
+.directive('paneHeading', [function() {
+  return {
+    restrict: 'EA',
+    template: '', //In effect remove this element
+    transclude: true,
+    replace: true,
+    compile: function(elm, attrs, transclude) {
+      return function link(scope, elm, attrs) {
+        //We can't require paneCtrl here because <pane-heading> element
+        //ends up getting compiled outside pane directive, in the tab's
+        //content area
+        scope.paneHeading = transclude(scope, angular.noop);
+      };
+    }
+  };
+}])
+
+.directive('paneHeadingTransclude', [function() {
+  return {
+    restrict: 'A',
+    require: '^pane',
+    link: function(scope, elm, attrs, paneCtrl) {
+      scope.$watch('paneHeading', function(html) {
+        if (html) {
+          elm.html('');
+          elm.append(html);
+        }
+      });
+    }
+  };
+}])
+
+.directive('paneContentTransclude', [function() {
+  return {
+    restrict: 'A',
+    require: '^tabs',
+    link: function(scope, elm, attrs, tabsCtrl) {
+      scope.$watch(function() {
+        return tabsCtrl.selectedPane;
+      }, function(pane) {
+        if (pane) {
+          elm.html('');
+          elm.append(pane.paneContent);
+        }
+      });
+    }
   };
 }]);
